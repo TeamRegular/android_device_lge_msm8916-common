6d5c8bc045f6b381123362ff2fe2101c6d698d65
C457 <android.c357@gmail.com>
2015-07-07 23:41:34 -0500

    g4stylus: fix build error

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -34,14 +34,6 @@ LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := init.qcom.rc
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.qcom.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE            := ueventd.g4stylus.rc
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
