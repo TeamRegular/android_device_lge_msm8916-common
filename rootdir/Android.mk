72d1e1b0c6e3613cd88cece5ca4a82f5a3158915
C457 <android.c357@gmail.com>
2015-07-08 00:43:08 -0500

    init: copy bt script to correct location

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,6 +1,16 @@
 LOCAL_PATH := $(call my-dir)
 
+# Configuration scripts
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.qcom.bt.sh
+LOCAL_MODULE_TAGS       := optional eng
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := etc/init.qcom.bt.sh
+include $(BUILD_PREBUILT)
+
 # Init scripts
+
 include $(CLEAR_VARS)
 LOCAL_MODULE            := fstab.g4stylus
 LOCAL_MODULE_TAGS       := optional eng
@@ -22,14 +32,6 @@ LOCAL_MODULE            := init.g4stylus.usb.rc
 LOCAL_MODULE_TAGS       := optional eng
 LOCAL_MODULE_CLASS      := ETC
 LOCAL_SRC_FILES         := etc/init.g4stylus.usb.rc
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.qcom.bt.sh
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := etc/init.qcom.bt.sh
 LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
