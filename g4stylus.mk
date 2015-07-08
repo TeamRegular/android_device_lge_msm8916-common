b2d40f4c288a7e76b0f2e9eb971c8d46b0cf7f2b
C457 <android.c357@gmail.com>
2015-07-07 23:22:54 -0500

    g4stylus: add qualcomm netmgr/dsi/qmi configs

--- g4stylus.mk
+++ g4stylus.mk
@@ -143,6 +143,12 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     power.msm8916
 
+# Qualcomm
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/dsi_config.xml:system/etc/data/dsi_config.xml \
+    $(LOCAL_PATH)/configs/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/qmi_config.xml:system/etc/data/qmi_config.xml
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.g4stylus \
